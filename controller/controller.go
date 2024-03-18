b586a39032c3ed0c32c36a44ada1a1f30db3a2b2
A, Mallikarjun <mala@victoria.com>
2024-03-18 12:17:22 +0530

    feeded data

--- controller/controller.go
+++ controller/controller.go
@@ -51,14 +51,24 @@ func (s Service) SearchByText(c *gin.Context) {
 	// TODO call openAI
 
 	inputSlice := strings.Split(req[0], " ")
-	if strings.ToLower(inputSlice[0]) == "hello" || strings.ToLower(inputSlice[0]) == "hi" || strings.ToLower(inputSlice[0]) == "hey" {
+	if strings.ToLower(inputSlice[0]) == "hi" || strings.ToLower(inputSlice[0]) == "hey" {
 		m := model.Msg{
-			Msg: "Hello, How may I help you?" +
-				"Also visit https://www.victoriassecret.com to continue your shopping experience",
+			Msg: "Hello! How can I assist you today?",
 		}
 		c.JSON(http.StatusOK, m)
 		return
 	}
+	if strings.ToLower(inputSlice[0]) == "hello" {
+		m := model.Msg{
+			Msg: "Hi there! Welcome to Victoria's Secret! 😊 " +
+				"How can I assist you today? Whether you're looking for lingerie, " +
+				"swimwear, or beauty products, I'm here to help you find the perfect fit!",
+		}
+		c.JSON(http.StatusOK, m)
+		return
+
+	}
+
 	reqString := checkInputSlice(inputSlice)
 
 	v, ok := s.File[reqString]
@@ -80,22 +90,30 @@ func checkInputSlice(inputSlice []string) string {
 	for _, v := range inputSlice {
 		if strings.ToLower(v) == "lip" {
 			res = "show me some lip essentials for valentine's day"
-		} else if strings.ToLower(v) == "beauty" || strings.ToLower(v) == "perfumes" {
+		} else if strings.ToLower(v) == "beauty" || strings.ToLower(v) == "perfume" || strings.ToLower(v) == "perfumes" || strings.ToLower(v) == "gift" || strings.ToLower(v) == "wife's" || strings.Contains(strings.ToLower(v), "wife's") ||
+			strings.Contains(strings.ToLower(v), "birthday") || strings.Contains(strings.ToLower(v), "gift") {
 			res = "show me some beauty products"
-		} else if strings.ToLower(v) == "sleep" || strings.ToLower(v) == "sleepwear" {
+		} else if strings.ToLower(v) == "sleep" || strings.ToLower(v) == "sleepwear" || strings.ToLower(v) == "sleepwears" {
 			res = "show me some trending regular sleep wear"
 		} else if strings.ToLower(v) == "sportswear" || strings.ToLower(v) == "sports" {
 			res = "show me some comfortable sportswear for my regular performance activity"
-		} else if strings.ToLower(v) == "night" || strings.ToLower(v) == "nightwear" {
+		} else if strings.ToLower(v) == "night" || strings.ToLower(v) == "nightwear" || strings.ToLower(v) == "nightwears" {
 			res = "suggest me some night wear for my girls night out"
+		} else if strings.ToLower(v) == "looking" || strings.ToLower(v) == "specific" {
+			res = "I'm looking for a bra but have specific needs"
 		} else if strings.ToLower(v) == "bra" || strings.ToLower(v) == "bras" {
-			res = "show me some bras"
+			res = "I'm looking for a bra but have specific needs"
 		} else if strings.ToLower(v) == "lingerie" {
 			res = "show me some lingerie"
 		} else if strings.ToLower(v) == "panties" || strings.ToLower(v) == "panty" {
 			res = "show me some panties"
-		} else if strings.ToLower(v) == "swim" || strings.ToLower(v) == "beach" {
+		} else if strings.ToLower(v) == "swim" || strings.ToLower(v) == "beach" || strings.ToLower(v) == "miami" || strings.Contains(strings.ToLower(v), "beach") ||
+			strings.Contains(strings.ToLower(v), "trip") {
 			res = "show me some swim suits"
+		} else if strings.ToLower(v) == "cancer" || strings.ToLower(v) == "survivor" || strings.ToLower(v) == "survivor" {
+			res = "cancer survivor"
+		} else if strings.ToLower(v) == "check" || strings.ToLower(v) == "checkout" || strings.ToLower(v) == "help" {
+			res = "I will check it out, thanks for your help"
 		}
 	}
 	return res
